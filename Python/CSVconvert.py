a9e434dfdbd4a106b0419575f26dc1f0d72a5fe1
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-14 23:03:52 -0500

    Remove failed approach

--- Python/CSVconvert.py
+++ Python/CSVconvert.py
@@ -42,13 +42,14 @@ countries = pd.read_fwf('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/text_f
                 widths=[12,9,9,5,5,5], header=None)
 
 # Convert to csv for PG import
-countries.to_csv('/Users/mm19864/Documents/BU_assignments/CS779/TermProj/NOAA_BigData/data/ghcnd-inventory.csv',
+countries.to_csv('C:/Users/dell/Documents/GitHub/NOAA_BigData/data/ghcnd-inventory.csv',
                    sep=',', encoding='utf-8', header=None)
 
 #################################
 
 # Concat all .dly files in directory together
 # Credit: https://stackoverflow.com/questions/17749058/
+
 read_files = glob.glob("D:/ghcnd_all/*.dly")
 
 with open("D:/result.txt", "wb") as outfile:
@@ -68,18 +69,3 @@ for chunk in pd.read_fwf('D:/result.txt',
     
     chunk.to_csv('D:/ghcnd-all.csv',
                        mode='a', sep=',', encoding='utf-8', header=None)
-    
-    
-#################################
-    
-# Another approach: convert to .csv FIRST, then concat
-
-## Under construction
-for file in read_files:
-    pd.read_fwf(file,
-                widths=[11,4,2,4,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1
-                        ,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1
-                        ,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1,5,1,1,1],
-                        header=None)
-    file.to_csv('D:/ghcnd_csv', sep=',', encoding='utf-8', header=None)
-
