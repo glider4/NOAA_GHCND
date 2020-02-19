2b74e727c6d5d8754726f52b1fa9c3fd2e810b8b
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-19 14:15:42 -0500

    No need to join Countries if not using CountryLong

--- SQL/NOAA_queryAVERAGES.sql
+++ SQL/NOAA_queryAVERAGES.sql
@@ -8,7 +8,7 @@ SELECT stationid, stateabbr, month, year, v1,v2,v3,v4,v5,v6,v7,v8,
 		v9,v10,v11,v12,v13,v14,v15,v16,v17,v18,v19,v20,v21,v22,v23,v24,v25,v26,v27,v28,v29,v30,v31
 FROM obs
 INNER JOIN Stations USING(StationID)
-INNER JOIN Countries USING(CountryAbbr)
+--INNER JOIN Countries USING(CountryAbbr)
 WHERE Element IN('TMAX')
 	--AND StateAbbr = 'FL'
 	--AND CountryAbbr = 'US'
