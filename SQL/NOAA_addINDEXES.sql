04833f28d27856d62766d7ca8c1e797fb0d131e7
mathemacode <43391344+mathemacode@users.noreply.github.com>
2020-02-21 09:19:44 -0500

    With StationsID as FK, no need for index

--- SQL/NOAA_addINDEXES.sql
+++ SQL/NOAA_addINDEXES.sql
@@ -1,9 +1,6 @@
 -- Implement indexes on NOAA DB
 
 CREATE INDEX stations_idx1
-ON STATIONS(StationID);
-
-CREATE INDEX stations_idx2
 ON STATIONS(CountryAbbr);
 
 CREATE INDEX countries_idx1
@@ -11,6 +8,3 @@ ON Countries(CountryAbbr);
 
 CREATE INDEX inventory_idx1
 ON INVENTORY(StationID);
-
-CREATE INDEX obs_idx1
-ON OBS(StationID);
